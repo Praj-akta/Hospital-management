053c39abd1c24695a6b2539d6585ac58686ba181
Neel <neel@gmail.com>
2023-02-08 18:56:51 -0500

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import Login from './login';
-import Home from './home';
-import { Route,BrowserRouter as Router, Routes} from 'react-router-dom';
-import './App.css';
-import { Route,BrowserRouter as Router, Routes } from 'react-router-dom';
-import Register from './register';
-import Home from './home'
+import Login from "./login";
+import Home from "./home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Register from "./register";
+import Home from "./home";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home />}>
-           
-        </Route>
-        <Route path="/login" element={<Login/>}>
-            
-        </Route>
-        <Route  path='/' element={
-            <Register/>
-          }>
-            
-          </Route>
-
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<Register />}></Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
