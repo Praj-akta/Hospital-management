b94d3a30ef79a63cc66cd90b001da4218de421dd
Tesha Patel <tpatel1937@conestogac.on.ca>
2023-02-08 18:27:12 -0500

    change in login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import Login from './login';
+import Home from './home';
+import { Route,BrowserRouter as Router, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Routes>
+        <Route exact path="/" element={<Home />}>
+           
+        </Route>
+        <Route path="/login" element={<Login/>}>
+            
+        </Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div>chcfhcfhcfh</div>
+  )
+}
+
+export default home
