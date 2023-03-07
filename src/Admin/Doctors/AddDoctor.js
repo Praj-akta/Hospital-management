67d3f6f5e8c4c9174bab81610ba1d3a59cd8ec07
Tesha Patel <tpatel1937@conestogac.on.ca>
2023-03-07 13:06:04 -0500

    changes in adddoctors and manage doctors

--- src/Admin/Doctors/AddDoctor.js
+++ src/Admin/Doctors/AddDoctor.js
@@ -1,15 +1,42 @@
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { db } from "../../firebase";
 import AdminHeader from "../AdminHeader";
 import AdminSidebar from "../AdminSidebar";
 import "../index.scss";
 
 function AddDoctor() {
+  const navigate = useNavigate();
   const [docname, setDocname] = useState("");
   const [speciality, setSpeciality] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
   const [fees, setFees] = useState("");
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const doctor = {
+      name: docname,
+      address,
+      email,
+      phone,
+      fees,
+      speciality
+    }
+    try{
+      const docRef = await addDoc(collection(db, "doctors"),{
+        doctor:doctor,
+      });
+      console.log("Document written with ID:",docRef.id);
+      navigate("admin/doctors")  
+    }
+    catch(e){
+      console.error("Error adding document:",e)
+    }
+  }
+
 
   return (
     <div className="admin-dashboard">
@@ -24,7 +51,7 @@ function AddDoctor() {
             </p>
           </div>
           <div className="add_doc_container">
-            <form>
+            <form onSubmit={(e) => handleSubmit(e)}>
               <label>Name:</label>
               <br />
               <input
--- src/Admin/Doctors/ManageDoctor.js
+++ src/Admin/Doctors/ManageDoctor.js
@@ -1,10 +1,23 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
+import { db } from "../../firebase";
 import AdminHeader from "../AdminHeader";
 import AdminSidebar from "../AdminSidebar";
+import {FaEdit} from "react-icons/fa";
+import {MdDelete} from "react-icons/md";
 import "../index.scss";
 
+
 function ManageDoctor() {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    getDocs(collection(db, "doctors")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => doc.data().doctor);
+      setDoctors(newData);
+    });
+  }, []);
 
   return (
     <div className="admin-dashboard">
@@ -27,9 +40,32 @@ function ManageDoctor() {
                   <th>Doctor Name</th>
                   <th>Email</th>
                   <th>Phone No</th>
+                  <th>Fees</th>
                   <th>Action</th>
                 </tr>
               </thead>
+              {doctors && doctors.length > 0 
+                ? <tbody>
+                  {
+                    doctors.map((value, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{index + 1}</td>
+                          <td>{value.speciality}</td>
+                          <td>{value.name}</td>
+                          <td>{value.email}</td>
+                          <td>{value.phone}</td>
+                          <td>{value.fees}</td>
+                          <td>
+                            <FaEdit />
+                            <MdDelete />
+                          </td>
+                      </tr>
+                      )
+                    })
+                  }
+                  </tbody> 
+                : null}
             </Table>
           </div>
         </div>
