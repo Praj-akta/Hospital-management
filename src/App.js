fbb641d786aba21abc7a22bb1889b02f5fa1c468
Prajakta Limje <prajaktalimje13@gmail.com>
2023-03-28 12:46:52 -0400

    added stripe payment, error in submitting payment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import "@stripe/stripe-js";
 import User from "./User";
 import Admin from "./Admin";
 import Home from "./Home/home";
@@ -13,6 +13,7 @@ import DoctorDashboard from "./DoctorDashboard";
 import PatientLogin from "./Login/PatientLogin";
 import Appointments from "./User/Appointments";
 import AddDoctor from "./Admin/Doctors/AddDoctor";
+import Payment from "./User/BookAppointment/Payment";
 import BookAppointment from "./User/BookAppointment";
 import AdminAppointments from "./Admin/Appointments";
 import EditDoctors from "./Admin/Doctors/editDoctors";
@@ -20,7 +21,8 @@ import EditPatient from "./Admin/Patients/editPatients";
 import ManageDoctor from "./Admin/Doctors/ManageDoctor";
 import DoctorsAppointments from "./DoctorDashboard/Appointments";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import './App.css';
+import ReScheduleAppointment from "./DoctorDashboard/Appointments/ReSchedule";
+import "./App.css";
 
 function App() {
   return (
@@ -34,24 +36,42 @@ function App() {
           <Route path="/doctor-login" element={<DoctorLogin />}></Route>
           <Route exact path="/register" element={<Register />}></Route>
           <Route path="/change-password" element={<ChangePassword />}></Route>
+          
           {/* admin routes */}
           <Route exact path="/admin/dashboard" element={<Admin />}></Route>
           <Route exact path="/admin/patients" element={<Patients />}></Route>
           <Route path="/admin/patients/edit" element={<EditPatient />}></Route>
           <Route exact path="/admin/doctors" element={<ManageDoctor />}></Route>
           <Route exact path="/admin/add-doctor" element={<AddDoctor />}></Route>
-          <Route exact path="/admin/doctors/edit" element={<EditDoctors />}></Route>
-          <Route exact path="/admin/appointments" element={<AdminAppointments />}></Route>
-          
+          <Route
+            exact
+            path="/admin/doctors/edit"
+            element={<EditDoctors />}
+          ></Route>
+          <Route
+            exact
+            path="/admin/appointments"
+            element={<AdminAppointments />}
+          ></Route>
+
           {/* user routes */}
           <Route path="/user" element={<User />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/appointments" element={<Appointments />}></Route>
           <Route path="/book-appointment" element={<BookAppointment />}></Route>
+          <Route path="/book-appointment/payment" element={<Payment />}></Route>
+
           {/* doctor dashboard routes */}
           <Route path="/doctor-dashboard" element={<DoctorDashboard />}></Route>
-          <Route path="/doctor-dashboard/appointments" element={<DoctorsAppointments />}></Route>
-        </Routes> 
+          <Route
+            path="/doctor-dashboard/appointments"
+            element={<DoctorsAppointments />}
+          ></Route>
+          <Route
+            path="/doctor-dashboard/reschedule-appointment"
+            element={<ReScheduleAppointment />}
+          ></Route>
+        </Routes>
       </div>
     </Router>
   );
--- src/DoctorDashboard/Appointments/index.js
+++ src/DoctorDashboard/Appointments/index.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import { Table } from "react-bootstrap";
+import { BsEyeFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { GrFormSchedule } from "react-icons/gr";
 import Sidebar from "../../components/Dashboard/Sidebar";
 import { collection, getDocs } from "firebase/firestore";
 import DashboardHeader from "../../components/Dashboard/DashboardHeader";
 import "./index.scss";
 
 function Appointments() {
+  const navigate = useNavigate();
   const [list, setList] = useState(null);
   const currentEmail = localStorage.getItem("email");
+
   useEffect(() => {
     getDocs(collection(db, "appointments")).then((querySnapshot) => {
-      const _data = querySnapshot.docs.map((doc) => doc.data());
+      const _data = querySnapshot.docs.map((doc) => {
+        return {
+          ...doc.data(),
+          id: doc.id
+        }
+      });
       const updatedData = _data.filter(
         ({ doctorEmail }) => doctorEmail === currentEmail
       );
@@ -37,16 +47,16 @@ function Appointments() {
                 <tr>
                   <th>#</th>
                   <th>Patient Name</th>
-                  <th>Date of Birth</th>
+                  <th>DOB</th>
                   <th>Email</th>
-                  <th>Appointment Date</th>
-                  <th>Appointment Time</th>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Actions</th>
                 </tr>
               </thead>
               {list && list.length > 0 ? (
                 <tbody>
                   {list.map((value, index) => {
-                    console.log(value)
                     return (
                       <tr key={index}>
                         <td>{index + 1}</td>
@@ -55,6 +65,30 @@ function Appointments() {
                         <td>{value.userDetails.email}</td>
                         <td>{value.date}</td>
                         <td>{value.appointmentTime}</td>
+                        <td>
+                          <BsEyeFill
+                            size="20"
+                            color="green"
+                            onClick={() =>
+                              navigate("/doctor-dashboard/patients-details")
+                            }
+                          />
+                          {"   "}
+                          <GrFormSchedule
+                            size="25"
+                            color="blue"
+                            onClick={() =>
+                              navigate(
+                                "/doctor-dashboard/reschedule-appointment",
+                                {
+                                  state: {
+                                    ...value
+                                  },
+                                }
+                              )
+                            }
+                          />
+                        </td>
                       </tr>
                     );
                   })}
